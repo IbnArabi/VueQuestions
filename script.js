2eb5e7b30989cb48e0fc9cad219c5964492affd5
GoodCoder09878 <62258613+GoodCoder09878@users.noreply.github.com>
2020-09-03 11:00:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Enter you good name to see a nice greeting'
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Enter your name to ask for your health'
+  }
+})
